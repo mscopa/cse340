aa3b3631c2116e3d1a846380cd861596b7c17ee3
mscopa <maurocopa20@gmail.com>
2025-04-03 00:53:27 -0300

    adding handleErrors to different routes

--- routes/errorRoute.js
+++ routes/errorRoute.js
@@ -2,8 +2,9 @@
 const express = require("express");
 const router = express.Router();
 const errorController = require("../controllers/errorController");
+const utilities = require("../utilities/");
 
 // Route to handle errors
-router.get("/trigger-error", errorController.triggerError);
+router.get("/trigger-error", utilities.handleErrors(errorController.triggerError));
 
 module.exports = router;
